a942ab2280d763c13ebccedf5fcbcb1fd7138a00
irfius <s.irfan@gmail.com>
2014-08-06 15:46:48 -0400

    functional test now checks to see if we can enter a to-do list

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -6,7 +7,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(5)
 
     def tearDown(self):
         self.browser.quit()
@@ -17,6 +18,26 @@ class NewVisitorTest(unittest.TestCase):
 
         #check for title
         self.assertIn('To-Do', self.browser.title)
+
+        #check if h1 tag contains "To-Do"
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        #check for a input to add to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item')
+
+        #type "Buy peacock feathers" in a text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        # enter text and check if page updates with list item in a table
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'Buy peacock feathers' for row in rows))
 
 if __name__ == '__main__':
     unittest.main()
