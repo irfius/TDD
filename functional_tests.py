50f1603f393cac9a9149f06703e17d93c064efe0
irfius <s.irfan@gmail.com>
2014-08-06 14:15:49 -0400

    updated FT with unittest

--- functional_tests.py
+++ functional_tests.py
@@ -3,4 +3,20 @@ import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #open browser
+        self.browser.get('http://localhost:8000')
+
+        #check for title
+        self.assertIn('To-Do', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main()
